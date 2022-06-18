ba224f8210fa51ba539bdac791c7716899790763
Thomas Bockhorn <historian.thomas@gmail.com>
2022-06-17 21:10:23 -0500

    Created a test if the painting images migration works

--- tests/Unit/PaintingImagesTest.php
+++ tests/Unit/PaintingImagesTest.php
@@ -3,14 +3,26 @@
 namespace Tests\Unit;
 
 use App\Models\PaintingImages;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class PaintingImagesTest extends TestCase
 {
+
+    /**
+     * This test will see if the painting images model exists
+     * @return void
+     */
     public function test_to_see_if_painting_images_model_exists()
     {
         $paintingImages = new PaintingImages();
 
         $this->assertInstanceOf(PaintingImages::class, $paintingImages);
+    }
+
+    public function test_to_see_if_painting_images_database_has_10_entries()
+    {
+        $this->seed();
+
+        $this->assertDatabaseCount("painting_images", 10);
     }
 }

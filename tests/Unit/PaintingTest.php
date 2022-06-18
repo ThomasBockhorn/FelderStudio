2b358943481896661b079b14489166822c18aec4
Thomas Bockhorn <historian.thomas@gmail.com>
2022-06-17 21:03:53 -0500

    Created a test if the painting migration works

--- tests/Unit/PaintingTest.php
+++ tests/Unit/PaintingTest.php
@@ -2,20 +2,33 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 use App\Models\Painting;
 
 class PaintingTest extends TestCase
 {
-
+    use RefreshDatabase;
     /**
      * This simple test will check to see if the Painting Model exists
      * @return void
      */
-    public function testIfPaintingModelExists()
+    public function test_If_Painting_Model_Exists()
     {
         $painting = new Painting();
 
         $this->assertInstanceOf(Painting::class, $painting);
+    }
+
+
+    /**
+     * This test will check if migration of the painting model works
+     * @return void
+     */
+    public function test_if_painting_database_table_has_10_entries()
+    {
+        $this->seed();
+
+        $this->assertDatabaseCount('paintings', 10);
     }
 }
